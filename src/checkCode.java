3565a394e54a69b0a3427848e4b5d4d62de89544
mike2151 <redefiningproductions@gmail.com>
2018-03-29 15:35:16 -0400

    version 1 finished

--- src/checkCode.java
+++ src/checkCode.java
@@ -1,37 +1,32 @@
 import com.intellij.find.FindManager;
 import com.intellij.find.FindModel;
 import com.intellij.find.FindSettings;
-import com.intellij.find.findInProject.FindInProjectManager;
 import com.intellij.find.impl.FindInProjectUtil;
 import com.intellij.find.impl.FindManagerImpl;
-import com.intellij.notification.NotificationGroup;
-import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.graph.option.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.usages.*;
 import com.intellij.util.Processor;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class checkCode extends AnAction {
-
-
     Project project;
 
     //list of likely phrases that involve API keys
     ArrayList<String> apiKeyPhrases = new ArrayList<String>();
 
+    String searchString = "";
+
 
     public checkCode() {
         // Set the menu item name.
-        super("Check Code");
+        super("Check For API Key Usages");
     }
 
     public void actionPerformed(AnActionEvent event) {
@@ -49,8 +44,11 @@ public class checkCode extends AnAction {
         findModel.setOpenInNewTab(false);
 
         //set up strings to find that will likely be API Keys.
-        //TODO figure out how to search multiple phrases
-        findModel.setStringToFind("yo");
+        populateWords();
+
+        searchString = setUpString(apiKeyPhrases);
+
+        findModel.setStringToFind(searchString);
 
 
         initModel(findModel, dataContext);
@@ -63,17 +61,9 @@ public class checkCode extends AnAction {
 
             startFindInProject(findModel);
 
-            findModel.setOpenInNewTabVisible(false); //todo check it in both cases: dialog & popup
+            findModel.setOpenInNewTabVisible(false);
         });
     }
-    /*
-    static void showNotAvailableMessage(AnActionEvent e, Project project) {
-        final String message = "'" + e.getPresentation().getText() + "' is not available while search is in progress";
-        NOTIFICATION_GROUP.createNotification(message, NotificationType.WARNING).notify(project);
-    }*/
-
-
-
 
 
     public void startFindInProject(FindModel findModel) {
@@ -136,6 +126,21 @@ public class checkCode extends AnAction {
         else {
             FindInProjectUtil.initStringToFindFromDataContext(findModel, dataContext);
         }
+    }
+
+    private String setUpString( List<String> words) {
+        String barSeparatedWords = "";
+        for (String word : words ) {
+            barSeparatedWords = barSeparatedWords + (word + "|");
+        }
+        //remove last bar in regex search phrase
+        barSeparatedWords = barSeparatedWords.substring(0, barSeparatedWords.length() - 1);
+        return "\\b(?:" + barSeparatedWords + ")\\b";
+    }
+
+    private void populateWords() {
+        apiKeyPhrases.add("key");
+        apiKeyPhrases.add("API");
     }
 
 
